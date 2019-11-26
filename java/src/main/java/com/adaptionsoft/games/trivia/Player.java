b46864e4169873d16c654ee894c8dfcf88587246
Philippe Bourgau <philippe.bourgau@murex.com>
2019-11-26 11:43:06 +0100

    Print player name in toString()

--- java/src/main/java/com/adaptionsoft/games/trivia/Player.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/Player.java
@@ -1,6 +1,14 @@
 package com.adaptionsoft.games.trivia;
 
 public class Player {
+    private final String name;
+
     public Player(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
--- java/src/test/java/com/adaptionsoft/games/trivia/PlayerTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/PlayerTest.java
@@ -2,10 +2,12 @@ package com.adaptionsoft.games.trivia;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PlayerTest {
 
     @Test
     public void create_a_new_player_with_a_name() {
-        new Player("Joe");
+        assertThat(new Player("Joe").toString()).isEqualTo("Joe");
     }
 }
