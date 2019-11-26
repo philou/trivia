66f9986ad349a817a203e6269cde9554d5080c93
Philippe Bourgau <philippe.bourgau@murex.com>
2019-11-26 12:04:36 +0100

    Prepare Golden Master Test with snippet and config

---
+++ java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -0,0 +1,24 @@
+package com.adaptionsoft.games.trivia.runner;
+
+import org.approvaltests.Approvals;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+public class GameRunnerTest {
+
+//    @Test
+//    public void golden_master_test() throws Exception {
+//        // NOTE: set coverage config to packages com.adaptionsoft.games.*
+//
+//        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        try (PrintStream ps = new PrintStream(baos, true, "UTF-8")) {
+//            GameRunner.main(new String[]{});
+//        }
+//        String output = new String(baos.toByteArray(), StandardCharsets.UTF_8);
+//
+//        Approvals.verify(output);
+//    }
+}
