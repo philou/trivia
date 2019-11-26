ea5f9c8b6322484012a3afc5ff92f4bde2f0fdf2
Philippe Bourgau <philippe.bourgau@murex.com>
2019-11-26 11:39:12 +0100

    Create Player Class

---
+++ java/src/main/java/com/adaptionsoft/games/trivia/Player.java
@@ -0,0 +1,6 @@
+package com.adaptionsoft.games.trivia;
+
+public class Player {
+    public Player(String name) {
+    }
+}
---
+++ java/src/test/java/com/adaptionsoft/games/trivia/PlayerTest.java
@@ -0,0 +1,11 @@
+package com.adaptionsoft.games.trivia;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void create_a_new_player_with_a_name() {
+        new Player("Joe");
+    }
+}
